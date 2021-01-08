54ec8766172fbbba23fdddf812ed9314350f2e7b
spences10 <spences10apps@gmail.com>
2021-01-08 16:36:27 +0000

    add globby and script for sitemap

--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ module.exports = {
   images: {
     domains: ['cdn.sanity.io'],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require('./scripts/generate-sitemap')
+    }
+
+    return config
+  },
 }
